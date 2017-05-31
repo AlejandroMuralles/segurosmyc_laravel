88fbe18969ac4cae3c6f3a6700a421164b3be38a
Alejandro Muralles <alejandromuralles57@gmail.com>
2017-05-30 22:21:11 -0600

    cambio en handler exceptions

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -44,6 +44,16 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($exception instanceof ModelNotFoundException) {
+            $exception = new NotFoundHttpException($exception->getMessage(), $exception);
+        }
+        if ($exception instanceof \App\App\Managers\ValidationException) {
+            return Redirect::back()->withInput()->withErrors($exception->getErrors());
+        }
+        if ($exception instanceof \App\App\Managers\SaveDataException) {
+            Session::flash('error', $exception->getException()->getMessage());
+            return Redirect::back()->withInput()->withErrors($exception->getException()->getMessage());
+        }
         return parent::render($request, $exception);
     }
 
