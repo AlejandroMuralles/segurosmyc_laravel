754e19f4016ead9bd1b0ff0b77de86b4f39474b5
Alejandro Muralles <alejandromuralles57@gmail.com>
2017-06-02 17:09:31 -0600

    cambio reporte renovacion

--- resources/views/reportes/polizas/renovacion.blade.php
+++ resources/views/reportes/polizas/renovacion.blade.php
@@ -1,59 +1,95 @@
 @extends('layouts.reporte')
 @section('title')
   Renovación de Póliza <br/> {{$poliza->numero}}
-@stop
+@endsection
+@section('css')
+<style type="text/css">
+
+table td{
+  font-size: 10px !important;
+  padding-top: 1px !important;
+  padding-bottom: 1px !important;
+  color: black !important;
+}
+table th{
+  font-size: 10px !important;
+  padding-top: 1px !important;
+  padding-bottom: 1px !important;
+}
+table{
+  border-collapse: collapse !important;
+  margin-bottom: 5px !important;
+  margin-top: 5px !important;
+}
+h6{
+  margin-top: 0 !important;
+}
+
+</style>
+@endsection
 @section('content')
   <div id="content">
-    <h4 class="title">Datos Generales de la Póliza</h4>
     <div class="ribbon bg-maroon"></div>
-    <table class="table table-bordered" style="margin-top: 3px">
+    <table class="table table-bordered">
       <tbody>
         <tr>
-          <td colspan="4">
+          <td colspan="6">
             Solicitud No.: {{$poliza->numero_solicitud}}
           </td>
-          <td colspan="8">
-            Solicita: {{$poliza->usuario_actualiza->colaborador->nombre_completo}}
-          </td>
-        </tr>
-        <tr>
-          <td colspan="12">
+          <td colspan="6">
             Fecha de Vigencia:
             desde {{ date('d-m-Y', strtotime($poliza->fecha_inicio)) }} hasta {{date('d-m-Y', strtotime($poliza->fecha_fin)) }}
           </td>
         </tr>
+        <tr>
+          <td colspan="4">
+            Solicita: {{$poliza->usuario_actualiza->colaborador->nombre_completo}}            
+          </td>
+          <td colspan="4">
+            Fecha de Solicitud: {{date('d-m-Y')}}
+          </td>
+          <td colspan="4">
+            Cantidad de Pagos: {{$poliza->cantidad_pagos}}
+          </td>
+        </tr>
+        <tr>
+          <td colspan="6">
+            Aseguradora: {{$poliza->aseguradora->nombre}}
+          </td>
+          <td colspan="6">
+            Dirigida a: {{$poliza->dirigida_a}}
+          </td>
+        </tr>
       </tbody>
     </table>
-    <h4 class="title">Datos del Asegurado</h4>
+    <h6 class="title">DATOS DEL ASEGURADO</h6>
     <div class="ribbon bg-maroon"></div>
-    <table class="table table-bordered" style="margin-top: 3px">
+    <table class="table table-bordered">
       <tbody>
         <tr>
           <td colspan="12">
-            Nombres y apellidos o Razón Social:  
+            @if($poliza->cliente->tipo_cliente == 'C')
+            Nombres y Apellidos:  
+            @else
+            Razón Social:
+            @endif
             {{$poliza->cliente->nombre}}
           </td>
         </tr>
         <tr>
-          <td colspan="5">
-            DPI/Pasaporte: {{$poliza->cliente->dpi}}
-          </td>
-          <td colspan="4">
-            NIT: {{$poliza->cliente->nit}}
-          </td>
-          <td colspan="3">
-            Sexo: {{$poliza->cliente->descripcion_genero}}
-          </td>
+          <td colspan="4">DPI/Pasaporte: {{$poliza->cliente->dpi}}</td>
+          <td colspan="4">NIT: {{$poliza->cliente->nit}}</td>
+          <td colspan="4">Sexo: @if($poliza->cliente->tipo_cliente == 'C') {{$poliza->cliente->descripcion_genero}} @endif</td>
         </tr>
         <tr>
-          <td colspan="4">Estado Civil <br/></td>
-          <td colspan="4">Fecha de Nacimiento: <br/> {{ date('d-m-Y', strtotime($poliza->cliente->fecha_nacimiento)) }}</td>
-          <td colspan="4">Lugar de Nacimiento: <br/> {{ $poliza->cliente->paisFiscal->nombre }} </td>
+          <td colspan="4">Estado Civil: </td>
+          <td colspan="4">Fecha de Nacimiento: {{ date('d-m-Y', strtotime($poliza->cliente->fecha_nacimiento)) }}</td>
+          <td colspan="4">Lugar de Nacimiento: {{ $poliza->cliente->paisFiscal->nombre }} </td>
         </tr>
         <tr>
-          <td colspan="4">Tipo de Actividad: <br/> {{$poliza->cliente->tipo_actividad}} </td>
-          <td colspan="4">Profesión: <br/> {{$poliza->cliente->profesion}}</td>
-          <td colspan="4">Oficio: <br/> {{$poliza->cliente->oficio}}</td>
+          <td colspan="4">Tipo de Actividad: {{$poliza->cliente->tipo_actividad}} </td>
+          <td colspan="4">Profesión: {{$poliza->cliente->profesion}}</td>
+          <td colspan="4">Oficio: {{$poliza->cliente->oficio}}</td>
         </tr>
         <tr>
           <td colspan="12">Dirección: {{$poliza->cliente->direccion_fiscal}} @if(!is_null($poliza->cliente->zona_fiscal))
@@ -66,40 +102,44 @@
         </tr>
       </tbody>
     </table>    
-    <h4 class="title">Datos de Facturación</h4>
+    <h6 class="title" style="margin-top: 0px">DATOS DE FACTURACIÓN</h6>
     <div class="ribbon bg-maroon"></div>
-    <table class="table table-bordered" style="margin-top: 0px">
+    <table class="table table-bordered" >
       <tbody>
         <tr>
-          <td colspan="12">
-            Nombres y apellidos o Razón Social:  {{$poliza->cliente->nombre_facturacion}}
+          <td colspan="8">
+            @if($poliza->cliente->tipo_cliente == 'C')
+            Nombres y Apellidos:  
+            @else
+            Razón Social:
+            @endif
+            {{$poliza->cliente->nombre_facturacion}}
           </td>
-        </tr>
-        <tr>
-          <td colspan="12">
+          <td colspan="4">
             NIT: {{$poliza->cliente->nit}}
           </td>
         </tr>
         <tr>
-          <td colspan="12">
+          <td colspan="8">
             Dirección: {{$poliza->cliente->direccion_facturacion}} 
             @if(!is_null($poliza->cliente->zona_facturacion))
               Zona {{$poliza->cliente->zona_facturacion}}
             @endif
           </td>
+          <td colspan="4">
+            Teléfono: {{$poliza->cliente->telefonos}}
+          </td>
         </tr>
       </tbody>
     </table>
-    <h4 style="page-break-before: always;" class="title">Datos de Vehículos</h4>
+    <h6 class="title">DATOS DE VEHÍCULOS</h6>
     <div class="ribbon bg-maroon"></div>
     @foreach($vehiculos as $v)
+      <h6 style="padding: 2px 5px !important; margin: 5px 0 0 0 !important; background-color: #0b2037; color: white"> 
+      NUMERO DE PLACA: {{ $v->vehiculo->placa }}
+      </h6>
       <table class="table table-bordered">
-        <thead>
-          <tr>
-            <th colspan="8" class="title"> Numero de Placa: {{ $v->vehiculo->placa }} </th>
-          </tr>
-        </thead>
-        <tbody> 
+        <tbody>
           <tr>
             <td colspan="2">Marca: {{ $v->vehiculo->marca->nombre }} </td>
             <td colspan="2">Línea: {{ $v->vehiculo->linea }} </td>
@@ -121,19 +161,18 @@
           </tr>    
         </tbody>
       </table>
+      <h6 style="padding: 2px 5px !important; margin: 0 !important; background-color: #0b2037; color: white"> VALORES</h6>
       <table class="table table-bordered">
-        <thead>
-          <tr>
-            <th colspan="10" class="title"> Valores </th>
-          </tr>
-        </thead>
         <tbody> 
           <tr>
-            <td colspan="2">Suma Asegurada: <br/>Q. {{ number_format($v->suma_asegurada,2) }} </td>
-            <td colspan="2">Prima Neta: <br/>Q. {{ number_format($v->prima_neta,2) }} </td>
-            <td colspan="2">Prima Total: <br/>Q. {{ number_format($v->prima_total,2) }} </td>
-            <td colspan="2">Deducible de Daños: <br/> {{ $v->pct_deducible_dano }}% </td>
-            <td colspan="2">Deducible de Robo: {{ $v->pct_deducible_robo }}% </td>
+            <td colspan="6">Suma Asegurada: Q. {{ number_format($v->suma_asegurada,2) }} </td>
+            <td colspan="6">Suma Asegurada Blindaje: Q. {{ number_format($v->suma_asegurada_blindaje,2) }} </td>
+          </tr>
+          <tr>
+            <td colspan="3">Prima Neta: Q. {{ number_format($v->prima_neta,2) }} </td>
+            <td colspan="3">Prima Total: Q. {{ number_format($v->prima_total,2) }} </td>
+            <td colspan="3">Deducible de Daños: {{ $v->pct_deducible_dano }}% </td>
+            <td colspan="3">Deducible de Robo: {{ $v->pct_deducible_robo }}% </td>
           </tr>
         </tbody>
       </table>
@@ -141,7 +180,7 @@
       <table class="table table-bordered">
         <tbody>
           <tr>
-            <td colspan="10" class="sub-title">Coberturas Particulares</td>
+            <td colspan="10" class="sub-title">COBERTURAS PARTICULARES</td>
           </tr>
           <tr>
             <th colspan="4" class="text-center">COBERTURA</th>
@@ -157,10 +196,9 @@
           @endforeach
         </tbody>
       </table>
-      @endif
-      <br/>      
+      @endif     
     @endforeach
-    <h2 style="page-break-before: always;" class="title">Coberturas Generales</h2>
+    <h6 class="title">COBERTURAS GENERALES</h6>
     <div class="ribbon bg-maroon"></div>
     <table class="table table-responsive">
       <thead>
